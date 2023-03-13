96d6bc4d79c5bcf95ce40ef8feb0a273e72cc82c
Rufat Karimov <ruffat_karimov@mail.ru>
2023-03-13 15:16:02 +0600

    sixs task

---
+++ task_6/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк: ");
+int n =int.Parse(ReadLine());
+
+double[,] Triangle = new double[n + 1, 2 * n + 1];
+
+FillPascalTriangle(Triangle);
+
+PascalTriangle(Triangle);
+
+WriteLine();
+GetArray(Triangle);
+
+void PascalTriangle(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    int count = 0;
+    for (int j = array.GetLength(1) - 1; j >= 0; j--)
+    {
+      if (array[i, j] != 0)
+      {
+        array[i, array.GetLength(1) / 2 + j - count] = array[i, j];
+        array[i, j] = 0;
+        count++;
+      }
+    }
+  }
+  array[array.GetLength(0) - 1, 0] = 1;
+}
+
+void FillPascalTriangle(double[,] pascalTriangle)
+{
+  for (int k = 0; k < pascalTriangle.GetLength(0); k++)
+  {
+    pascalTriangle[k, 0] = 1;
+  }
+  for (int i = 1; i < pascalTriangle.GetLength(0); i++)
+  {
+    for (int j = 1; j < i + 1; j++)
+    {
+      pascalTriangle[i, j] = pascalTriangle[i - 1, j] + pascalTriangle[i - 1, j - 1];
+    }
+  }
+}
+
+void GetArray(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i, j] != 0)
+      {
+          Write($"{array[i, j]} ");
+      }
+      else Write("  ");
+    }
+    WriteLine();
+  }
+}
+
