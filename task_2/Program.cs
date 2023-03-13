9da084a77f0594cf36cd17dd4c47e04c45e13eee
Rufat Karimov <ruffat_karimov@mail.ru>
2023-03-13 13:29:52 +0600

    sec task

---
+++ task_2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+int min = 0;
+int sum = SumElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int tempSum = SumElements(array, i);
+  if (sum > tempSum)
+  {
+    sum = tempSum;
+    min = i;
+  }
+}
+
+Console.WriteLine($"\n{min+1} - строкa с наименьшей суммой ({sum}) элементов ");
+
+
+int SumElements(int[,] array, int i)
+{
+  int sum = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sum += array[i,j];
+  }
+  return sum;
+}
+   
+ 
+
